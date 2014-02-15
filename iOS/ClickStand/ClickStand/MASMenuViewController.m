fe400a29dc947ad8ae4d12b525681612821cfa37
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-02-15 13:16:43 -0500

    yoloswag bri iz poop

---
+++ iOS/ClickStand/ClickStand/MASMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  MASMenuViewController.m
+//  ClickStand
+//
+//  Created by Matthew Ebeweber on 2/15/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import "MASMenuViewController.h"
+
+@interface MASMenuViewController ()
+
+@end
+
+@implementation MASMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
