e5cdee0811e0b25cc211a104df03445a43ce0dbf
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-02-14 20:47:46 -0500

    added Parse. awww yeahhh

--- iOS/ClickStand/ClickStand/MASAppDelegate.m
+++ iOS/ClickStand/ClickStand/MASAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "MASAppDelegate.h"
+#import <Parse/Parse.h>
 
 @implementation MASAppDelegate
 
@@ -15,6 +16,13 @@
     self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    // Parse ID and Key. Change after handing over to Clickstand
+    [Parse setApplicationId:@"hxjc7PsqUeQww2KZSMfMp3ZhFajFZHCT0JIPeR1t"
+                  clientKey:@"hNNr6ZZNrADDD5Cgxt5ThKc9GdB6GqUZQnk4Z1hB"];
+    
+    // Track statistics and analytics through Parse
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
