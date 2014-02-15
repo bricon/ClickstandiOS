1b394b14c836373b9ee0b695f37321fc789c1d89
Comyar <comyarzaheri@utexas.edu>
2014-02-15 13:40:24 -0500

    stuf yo

--- iOS/ClickStand/ClickStand/MASSideMenuViewController.m
+++ iOS/ClickStand/ClickStand/MASSideMenuViewController.m
@@ -75,14 +75,10 @@
     self.backgroundImageView.transform = CGAffineTransformIdentity;
     self.backgroundImageView.frame = self.view.bounds;
     self.menuController.view.frame = self.view.bounds;
-//    self.menuController.view.transform = CGAffineTransformMakeScale(1.5f, 1.5f);
-//    self.menuController.view.alpha = 0;
-//    self.backgroundImageView.transform = CGAffineTransformMakeScale(1.7f, 1.7f);
     
     [self.view.window endEditing:YES];
     
     [UIView animateWithDuration:0.3f animations:^{
-        self.contentController.view.transform = CGAffineTransformMakeScale(0.7f, 0.7f);
         self.contentController.view.center = CGPointMake(CGRectGetWidth(self.view.bounds), self.view.center.y);
         self.menuController.view.alpha = 1.0f;
         self.menuController.view.transform = CGAffineTransformIdentity;
@@ -100,9 +96,7 @@
     [UIView animateWithDuration:0.3f animations:^{
         self.contentController.view.transform = CGAffineTransformIdentity;
         self.contentController.view.frame = self.view.bounds;
-        self.menuController.view.transform = CGAffineTransformMakeScale(1.5f, 1.5f);
         self.menuController.view.alpha = 0;
-//        self.backgroundImageView.transform = CGAffineTransformMakeScale(1.7f, 1.7f);
         
         for (UIMotionEffect *effect in self.contentController.view.motionEffects) {
             [self.contentController.view removeMotionEffect:effect];
@@ -132,29 +126,7 @@
     
     if (gestureRecognizer.state == UIGestureRecognizerStateBegan || gestureRecognizer.state == UIGestureRecognizerStateChanged) {
         CGFloat delta = self.menuVisible ? (point.x + originalPoint.x) / originalPoint.x : point.x / self.view.frame.size.width;
-        
-        CGFloat contentViewScale = 1 - (0.3f * delta);
-//        CGFloat backgroundViewScale = 1.7f - (0.7f * delta);
-        CGFloat menuViewScale = 1.5f - (0.5f * delta);
-        
-        self.menuController.view.alpha = delta;
-        
-//        self.backgroundImageView.transform = CGAffineTransformMakeScale(backgroundViewScale, backgroundViewScale);
-        self.menuController.view.transform = CGAffineTransformMakeScale(menuViewScale, menuViewScale);
-        
-//        if (backgroundViewScale < 1) {
-//            self.backgroundImageView.transform = CGAffineTransformIdentity;
-//        }
-        
-        if (contentViewScale > 1) {
-            if (!self.menuVisible) {
-                self.contentController.view.transform = CGAffineTransformIdentity;
-            }
-            self.contentController.view.frame = self.view.bounds;
-        } else {
-            self.contentController.view.transform = CGAffineTransformMakeScale(contentViewScale, contentViewScale);
-            self.contentController.view.transform = CGAffineTransformTranslate(self.contentController.view.transform, point.x, 0);
-        }
+        self.contentController.view.transform = CGAffineTransformTranslate(self.contentController.view.transform, delta, 0);
     }
     
     if (gestureRecognizer.state == UIGestureRecognizerStateEnded) {
