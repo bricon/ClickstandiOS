20a199d200740518176900489d9f205137e4c8fb
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-02-16 04:12:21 -0500

    added other url, need to check and make sure cards are saving

--- routes.py
+++ routes.py
@@ -53,5 +53,30 @@ def card():
     # return flask.jsonify(**{success: 1})
 
 
+@app.route('/touch', methods=["POST"])
+def touch():
+    pdb.set_trace()
+    result = braintree.Transaction.sale({
+        "amount": "10.00",
+        "venmo_sdk_payment_method_code": params["venmo_sdk_payment_method_code"]
+    })
+
+    if result.is_success:
+        print "success!: " + result.transaction.id
+        return jsonify( {"success": 1} )
+    elif result.transaction:
+        print "Error processing transaction:"
+        print "  message: " + result.message
+        print "  code:    " + result.transaction.processor_response_code
+        print "  text:    " + result.transaction.processor_response_text
+    else:
+        print "message: " + result.message
+        for error in result.errors.deep_errors:
+            print "attribute: " + error.attribute
+            print "  code: " + error.code
+            print "  message: " + error.message
+
+    return jsonify( {"success": 0} )
+
 if __name__ == '__main__':
     app.run()
